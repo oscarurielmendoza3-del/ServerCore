005b31d5353fabde28c0020285048fbf9ecc37eb
diegofmo0802 <diegofmo0802@gmail.com>
2025-08-29 01:04:18 +0000

    chore(test): tests updated

--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,18 @@
-//@ts-check
+/// @ts-check
 
-import Rule from "../build/Server/Rule.js";
-import ServerCore, { Debug, Utilities } from "../build/ServerCore.js";
+import ServerCore, { Logger, Debug, Utilities } from "../build/ServerCore.js";
 import "./debug.js";
 import "./utilities.js";
 
-/* | HABILITAR VISTA EN CONSOLA DE LOS DEBUGS DE SERVER CORE | */
+/* | ENABLING ALL THE SERVER CORE LOGS IN CONSOLE | */
 Debug.showAll = true;
-/* | CREANDO UN DEBUG PARA LA SALIDA DE LOS TESTS | */
-const $Test = Debug.getInstance('Test');
+/* | CREATING DEBUG INSTANCE TO THE TESTS | */
+const debug = new Logger({ prefix: '[TEST]' });
 
-/* | CARGANDO VARIABLES DE ENTORNO DE PRUEBA | */
+/* | CREATING ENV VARIABLES FOR THE TESTS | */
 
 await Utilities.Env.load('tests/test.env');
+// Utilities.Env.loadSync('tests/test.env');
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT ?
@@ -21,28 +21,28 @@ Number(process.env.PORT)
 : 5050 : 5050;
 const WEBSOCKET_URL = process.env['REMOTE-WS'] ?? `ws://${HOST ?? 'localhost'}:${PORT}`;
 
-/* | INICIANDO SERVIDOR | */
+/* | CREATING SERVER | */
 const server = new ServerCore({ port: 3000 });
 
-/* | PRUEBAS DE REGLAS FILE | */
-server.addFile('/File', 'changes.md')
-server.addFile('/favicon.ico', 'Global/Source/Logo_SM_960.png');
+/* | FILE RULE TEST | */
+server.router.addFile('/File', 'changes.md')
+server.router.addFile('/favicon.ico', 'Global/Source/Logo_SM_960.png');
 
-/* | PRUEBAS DE REGLAS FILE usando AUTH | */
-server.addFile('/FileWA', 'changes.md',
+/* | FILE RULE TEST USING AUTH EXEC | */
+server.router.addFile('/FileWA', 'changes.md',
     (Request) => (Request.searchParams.Auth != null && Request.searchParams.Auth == 'AuthYes')
 )
 
-/* | CREANDO REGLAS CON EL CONSTRUCTOR Rule | */
-server.addRules(
-    new ServerCore.Rule('File', 'GET', '/MyFile/*', 'Test/Test.js', () => true)
+/* | CREATING RULES WITH THE RULE CONSTRUCTOR | */
+server.router.addRules(
+    new ServerCore.Router.Rule('File', 'GET', '/MyFile/*', 'Test/Test.js', () => true)
 );
 
-/* | PRUEBAS DE REGLAS FOLDER | */
-server.addFolder('/Folder', '.debug');
+/* | FOLDER RULES TEST | */
+server.router.addFolder('/Folder', '.debug');
 
-/* | PRUEBAS DE REGLAS ACTION | */
-server.addAction('ALL', '/', (Rq, Rs) => {
+/* | ACTION RULES TEST | */
+server.router.addAction('ALL', '/', (Rq, Rs) => {
     Rs.sendTemplate('tests/test.HSaml', {
         Tittle: '[Saml] · Tests',
         Sources: {
@@ -50,21 +50,21 @@ server.addAction('ALL', '/', (Rq, Rs) => {
             FileWithAuthFunction_NoAuth: '/FileWA',
             FileWithAuthFunction_Auth: '/FileWA?Auth=AuthYes',
             Folder: '/Folder',
-            RuleParams: '/RuleParams/algo1/otro2/nose3/XD',
+            RuleParams: '/RuleParams/param1/param2/param3/XD',
             WebSocket: '/WebSocket'
         }
     });
 });
 
-/* | PRUEBAS DE URL PARAMS | */
-server.addAction('ALL', '/RuleParams/$?param1/$?b/$?c/*', (Rq, Rs) => {
+/* | URL RULE PARAMS TEST | */
+server.router.addAction('ALL', '/RuleParams/$?param1/$?b/$?c/*', (Rq, Rs) => {
     Rs.sendJson({
         Url: Rq.url,
         RuleParams: Rq.ruleParams
     })
 });
 
-/* | PRUEBAS DE REGLAS WEB-SOCKET | */
+/* | WEBSOCKET RULE TEST | */
 /**
  * @typedef {import('../build/Server/WebSocket/WebSocket.js').WebSocket} WebSocket
  */
@@ -84,13 +84,13 @@ function broadCast(message, exclude = null) {
     })
 }
 
-server.addAction('ALL', 'WebSocket', (Rq, Rs) => {
+server.router.addAction('ALL', 'WebSocket', (Rq, Rs) => {
     Rs.sendTemplate('tests/websocket.HSaml', {
         Host: `${WEBSOCKET_URL}/WebSocket`
     });
 });
 
-server.addWebSocket('/WebSocket/$?username', (request, socket) => {
+server.router.addWebSocket('/WebSocket/$?username', (request, socket) => {
     const username = request.ruleParams.username;
     if (!username) {
         socket.send('error: no username');
@@ -104,26 +104,26 @@ server.addWebSocket('/WebSocket/$?username', (request, socket) => {
     }
     usernames.add(username)
     clients.add(socket);
-    $Test.log("[WebSocket]", "new client", username);
+    debug.log("[WebSocket]", "new client", username);
     
-    socket.send("server: hallo");
-    broadCast('server: nuevo cliente: ' + username, socket);
+    socket.send("[server] hallo");
+    broadCast('[server] new client: ' + username, socket);
 
     socket.on('message', (data, info) => {
         if (info.opCode !== 0x1) return;
         const message = username + ': ' + data.toString();
-        $Test.log(['[WebSocket]', message]);
+        debug.log(['[WebSocket]', message]);
         broadCast(message, socket);
     });
     socket.on('error', (Error) => {
-        $Test.log('[WebSocket-Error]: ' + Error.message);
+        debug.log('[WebSocket-Error]: ' + Error.message);
     });
     socket.on('finish', () => {
         clients.delete(socket);
         usernames.delete(username);
-        broadCast('server: cliente desconectado: ' + username);
-        $Test.log('[WebSocket-Finish]: client disconnected')
+        broadCast('[server] client disconnected: ' + username);
+        debug.log('[WebSocket-Finish]: client disconnected')
     });
 });
+
 server.start();
-// new Rule('Action', 'POST', '/a/b/c/d/e/$f/*', () => {});
